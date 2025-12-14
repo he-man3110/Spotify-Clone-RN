bbc1ffd7e188be889054194f5e32850981b1d18c
Hemanth <hemanthmb3110@gmail.com>
2025-12-14 11:33:18 +0530

    Fixed app config mismatch due to custom configs. Updated android icon & splash screen assets.

--- utils/AppConfig.ts
+++ utils/AppConfig.ts
@@ -1,4 +1,4 @@
-import configs from "../app.config.json";
+import configs from "../FeatureFlags";
 export namespace AppConfig {
   export function shouldEnableReduxLogs() {
     return configs.REDUX_LOGS === "true";
